fa8d7084889752043ff871df9f2ad53ac5107962
Eetu Raunio <eetu.raunio@helsinki.fi>
2023-06-29 20:14:30 +0300

    feat: Life animation

--- src/components/LifeDisplay.tsx
+++ src/components/LifeDisplay.tsx
@@ -1,8 +1,8 @@
 function LifeDisplay({ lives = 4 }) {
   return (
     <div className="lives">
-      {new Array(lives).fill(0).map((_, i) => (
-        <div key={i} className="life"></div>
+      {new Array(4).fill(0).map((_, i) => (
+        <div key={i} className={i >= lives ? "life life--lost" : "life"}></div>
       ))}
     </div>
   );
